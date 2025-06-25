667af1ddc1fde8fece2007e9d25c06514a7cd300
Smit79 <smitvansjaliya79@gmail.com>
2025-06-25 18:06:01 +0530

    userrouter and controller test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import cookieParse from 'cookie-parse'
-import e from 'express';
+import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use( cors ({
@@ -12,7 +11,13 @@ app.use( cors ({
 app.use(express.json({ limit : "16kb"}))
 app.use(express.urlencoded({ extended: true , limit : "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParse())
+app.use(cookieParser())
+
+// router import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/user" , userRouter)
 
 // app.on("error",(error) => {
 //     console.error("ERROR : " , error);
